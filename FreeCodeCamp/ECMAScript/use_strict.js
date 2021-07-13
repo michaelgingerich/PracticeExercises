2218ffe88533d63c28e718c1424518b15ccdd11e
Michael Gingerich <mmgwd5@umsystem.edu>
2021-07-13 17:39:56 -0500

    Included ECMAScript/use_strict.html and ECMAScript/use_strict.js

---
+++ FreeCodeCamp/ECMAScript/use_strict.js
@@ -0,0 +1,15 @@
+//var catName;
+//catName = "Oliver";
+//var quote;
+let catName;
+let quote;
+
+function catTalk() {
+  "use strict";
+  x = 3.14;
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
